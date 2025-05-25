1fcc838751a1de011977f56522f2d34b22734752
Sadman Rahman Maein <smeainrahman@gmail.com>
2025-05-25 22:27:38 +0600

    Delete VIEWS directory

--- VIEWS/includes/footer.php
+++
@@ -1,9 +0,0 @@
-    <!-- Common JavaScript -->
-    <script src="<?php echo JS_PATH; ?>main.js"></script>
-    
-    <!-- Page specific JavaScript -->
-    <?php if (isset($page_js)): ?>
-        <script src="<?php echo JS_PATH . $page_js; ?>"></script>
-    <?php endif; ?>
-</body>
-</html> 
