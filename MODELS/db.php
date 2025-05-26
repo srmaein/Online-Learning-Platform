a7b9d9867f83dc1028c89592841cfb1f25747a6a
Sadman Rahman Maein <smeainrahman@gmail.com>
2025-05-26 10:52:44 +0600

    Add files via upload

---
+++ MODELS/db.php
@@ -0,0 +1,27 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $database = "Online_Education";
+    private $conn;
+
+    public function __construct() {
+        $this->conn = new mysqli($this->host, $this->username, $this->password, $this->database);
+        
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
+    }
+
+    public function getConnection() {
+        return $this->conn;
+    }
+
+    public function __destruct() {
+        if ($this->conn) {
+            $this->conn->close();
+        }
+    }
+}
+?> 
